e71c8c68366e80f4d7be21ce9e40508a5feff241
TanguturuSucharitha <60088057+TanguturuSucharitha@users.noreply.github.com>
2020-03-02 20:48:38 -0800

    stage04 gatewayservice

---
+++ stage04-adb-gateway-service/src/test/java/com/cts/ms/Stage04AdbGatewayServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cts.ms;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class Stage04AdbGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
